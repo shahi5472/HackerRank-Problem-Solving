11b00e633d6234b2b419dc7745ab58d992a65dea
S.M. SHAHi <s.m.kamalhussain@gmail.com>
2022-01-30 13:37:14 +0600

    Java If-Else

---
+++ JavaIfElse.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class JavaIfElse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();            
+        if(n%2==1 || ( (n%2==0) && (n>=6 && n <= 20 ) ) ){
+         System.out.println("Weird");
+        }
+        else{
+            System.out.println("Not Weird");
+        }
+    }
+}
